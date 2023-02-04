dff81b47e2431ff1706c503dc9573086ead9494a
Jake <jakemccann2000@gmail.com>
2023-02-03 20:31:19 -0500

    Cannot successfuly read column

--- data_viewer.py
+++ data_viewer.py
@@ -1,4 +1,14 @@
 import PySimpleGUI as sg
+import main_splash
+import pandas as pd
+
+def regulate_data(data, header, desc_col_label, date_col_label, category_col_label, cost_col_label):
+    desc_col = data[header.index(desc_col_label)]
+    date_col = data[header.index( date_col_label)]
+    category_col = data[header.index(category_col_label)]
+    print(header.index(category_col_label))
+    cost_col = data[:]
+    print(cost_col)
 
 def create(filename, header, data):
     data_table = [
@@ -17,5 +27,6 @@ def create(filename, header, data):
         if event == "Continue":
             required_data_filled = values["-DESC-"] and values["-DATE-"] and values["-CATEGORY-"] and values["-COST-"]
             if required_data_filled:
-                break
+                normal_data = regulate_data(data, header, values["-DESC-"], values["-DATE-"], values["-CATEGORY-"], values["-COST-"])
+                main_splash.create(normal_data)
     window.close()
