c70320df0dafa80b34190db146392b89a5f68912
Jake <jakemccann2000@gmail.com>
2023-02-06 21:13:49 -0500

    get rid of pie

--- main_menu.py
+++ main_menu.py
@@ -1,14 +1,5 @@
 import PySimpleGUI as Gui
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
 import numpy as np
-
-
-def draw_figure(canvas, figure):
-    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-    return figure_canvas_agg
 
 
 def print_category_value(pct, all_vals):
@@ -16,28 +7,28 @@ def print_category_value(pct, all_vals):
     return "{:.1f}% (${:d})".format(pct, absolute)
 
 
-def create_pie(panda):
+def category_sums(panda):
     categories = set(panda["Category"])
     rem_categories = set()
-    data = []
+    cat_sums = []
     for cat in categories:
         cat_total = panda.loc[panda["Category"] == cat, "Cost"].sum()
         if cat_total != 0:
-            data.append(cat_total)
+            cat_sums.append(cat_total)
         else:
             rem_categories.add(cat)
 
     for cat in rem_categories:
         categories.remove(cat)
 
-    explode_vals = np.full(
-                      shape=len(data),
-                      fill_value=0.2,
-                      dtype=np.float
-                    )
-    figure = plt.figure()
-    plt.pie(data, labels=categories, autopct=lambda pct: print_category_value(pct, data), explode=explode_vals)
-    return figure
+    cat_to_sum = {}
+    for key in categories:
+        for value in cat_sums:
+            cat_to_sum[key] = value
+            cat_sums.remove(value)
+            break
+
+    return cat_to_sum
 
 
 def create(panda):
@@ -48,17 +39,13 @@ def create(panda):
     ]
     total_spent = panda.loc[:, "Cost"].sum()
     stat_menu = [
-        [Gui.Text("Total Spent:"), Gui.Text("$" + str(total_spent))],
-        [Gui.Canvas(key='-PIE-')]
+        [Gui.Text("Total Spent:"), Gui.Text("$" + str(total_spent))]
     ]
     layout = [
         [Gui.Column(data_table), Gui.VerticalSeparator(), Gui.Column(stat_menu)]
     ]
     window = Gui.Window("Spending Overview",
-                        layout,
-                        finalize=True)
-    pie_chart = create_pie(panda)
-    draw_figure(window['-PIE-'].TKCanvas, pie_chart)
+                        layout)
 
     while True:
         event, values = window.read()
